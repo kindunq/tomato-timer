d4118b09bc0a6e965409439805e95f6516fb601f
kindunq <humanb2box@gmail.com>
2019-01-03 18:12:47 +0900

    docs: Add provider

--- App.js
+++ App.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import { Platform, StatusBar, StyleSheet, View } from 'react-native';
 import { AppLoading, Asset, Font, Icon } from 'expo';
-import Timer from './components/Timer'
+import Timer from './components/Timer';
+import reducer from './reducer';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
 
-
+let store = createStore(reducer);
 
 export default class App extends React.Component {
-  render() {
-    return  <Timer/>
+  render(){
+    return(
+      <Provider store={store}>
+        <Timer/>
+      </Provider>
+    );
   } 
 }
