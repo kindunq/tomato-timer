1f152b6b7ad17fa4a88455f8237d0bb9f738605e
kindunq <humanb2box@gmail.com>
2019-02-22 18:29:08 +0900

    docs: add func formattime

--- components/Timer/presenter.js
+++ components/Timer/presenter.js
@@ -2,6 +2,21 @@ import React, {Component} from 'react';
 import {View, Text, StyleSheet, StatusBar} from 'react-native';
 import Button from "../Button";
 
+function formatTime(time) {
+    var minutes = Math.floor(time / 60);
+    time -= minutes * 60;
+  
+    var seconds = parseInt(time % 60, 10);
+  
+    return `${minutes < 10 ? `0${minutes}` : minutes}:${seconds < 10
+      ? `0${seconds}`
+      : seconds}`;
+  
+    return;
+  }
+  
+
+
 class Timer extends Component {
 
 componentWillReceiveProps(nextProps){
@@ -28,7 +43,7 @@ componentWillReceiveProps(nextProps){
             <View style={styles.container}>
                 <StatusBar barStyle={"light-content"}/>
                 <View style={styles.upper}>
-                    <Text style={styles.time}>25:00</Text>
+                    <Text style={styles.time}>{formatTime(timerDuration - elapsedTime)}</Text>
                 </View>
                 <View style={styles.lower}>
                     { !isPlaying ? <Button iconName="play-circle" onPress={startTimer} /> : <Button iconName="stop-circle" onPress={restartTimer} /> }
